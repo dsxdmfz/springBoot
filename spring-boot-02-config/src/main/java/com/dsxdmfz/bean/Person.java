db67a24ccd9f0f2837bc9104e2833a6c33fd400b
dsxdmfz <zhangle.lang@qq.com>
2018-04-24 23:05:10 +0800

    @ConfigurationProperties与@Value区别

--- spring-boot-02-config/src/main/java/com/dsxdmfz/bean/Person.java
+++ spring-boot-02-config/src/main/java/com/dsxdmfz/bean/Person.java
@@ -1,8 +1,11 @@
 package com.dsxdmfz.bean;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Email;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -11,13 +14,29 @@ import java.util.Map;
  * 将配置文件中配置的每一个属性的值，映射到这个组件中
  * @ConfigurationProperties 告诉spring boot 将本类中的所有属性和配置文件中相关的配置进行绑定
  *      prefix = "person" 配置文件中哪个下面的属性进行一一映射
+ *
+ * 只有这个组建是容器中的组件，才能容器提供的@ConfigurationProperties 功能
  */
 @Component
 @ConfigurationProperties(prefix = "person")
+//@Validated
 public class Person {
+
+    /**
+     * <bean class="Person">
+     *     <property name="lastName" value="字面量/${key}从环境变量、配置文件中获取值/#{SpEL}"></property>
+     * </bean>
+     */
+
+//    @Value("${person.name}")
+    // name必须是邮箱格式
+//    @Email
     private String name;
+//    @Value("#{12*2}")
     private Integer age;
     private Date brith;
+//    @Value("true")
+    private boolean boss;
     private Map<String,Object> map;
     private List<String> list;
     private Dog dog;
@@ -28,6 +47,7 @@ public class Person {
                 "name='" + name + '\'' +
                 ", age=" + age +
                 ", brith=" + brith +
+                ", boss=" + boss +
                 ", map=" + map +
                 ", list=" + list +
                 ", dog=" + dog +
@@ -56,6 +76,14 @@ public class Person {
 
     public void setBrith(Date brith) {
         this.brith = brith;
+    }
+
+    public boolean isBoss() {
+        return boss;
+    }
+
+    public void setBoss(boolean boss) {
+        this.boss = boss;
     }
 
     public Map<String, Object> getMap() {
