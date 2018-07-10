5e61058a06c8f594022f469a6765d3a4b6f8f107
dsxdmfz <zhangle.lang@qq.com>
2018-07-10 23:05:12 +0800

    thymeleaf语法

--- spring-boot-04-web-restfulcrud/src/main/java/com/dsxdmfz/controller/HelloController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/dsxdmfz/controller/HelloController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
 @Controller
 public class HelloController {
 
@@ -11,6 +15,14 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
         return "Hello world";
+    }
+
+    //查出一些数据，在页面显示
+    @RequestMapping("/success")
+    public  String success(Map<String,Object> map){
+        map.put("hello","<h1>你好</h1>");
+        map.put("users", Arrays.asList("lez001","lez002","lez003"));
+        return "success";
     }
 
 }
