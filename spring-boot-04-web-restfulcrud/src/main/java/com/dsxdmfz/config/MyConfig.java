7a691c6e7aea6ba6d1a09cb4e22ee7d0daef6cec
lez <534277194@qq.com>
2018-07-18 16:01:41 +0800

    【实验】-引入资源

--- spring-boot-04-web-restfulcrud/src/main/java/com/dsxdmfz/config/MyConfig.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/dsxdmfz/config/MyConfig.java
@@ -1,5 +1,6 @@
 package com.dsxdmfz.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,9 +9,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //但是，springboot2.0使用的spring5，Springboot2.0中WebMvcConfigurerAdapter过时，所以实现 WebMvcConfigurer接口来代替
 @Configuration
 public class MyConfig implements WebMvcConfigurer {
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         //浏览器发送 /dsxdmfz 请求来到 success
         registry.addViewController("/dsxdmfz").setViewName("success");
+    }
+
+    //使用WebMvcConfigurerAdapter过时，用WebMvcConfigurer代替
+    //所有的WebMvcConfigurer组件都会在一起起作用
+    @Bean//将组件注册在容器
+    public WebMvcConfigurer webMvcConfigurer(){
+        WebMvcConfigurer webMvcConfigurer = new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+            }
+        };
+        return webMvcConfigurer;
     }
 }
--- spring-boot-04-web-restfulcrud/src/main/java/com/dsxdmfz/controller/HelloController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/dsxdmfz/controller/HelloController.java
@@ -11,6 +11,11 @@ import java.util.Map;
 @Controller
 public class HelloController {
 
+//    @RequestMapping("/")
+//    public String index(){
+//        return "index";
+//    }
+
     @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
