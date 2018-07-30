3c0ef0f3e757a706f329c4330e644725fa286361
lez <534277194@qq.com>
2018-07-30 15:42:59 +0800

    【实验】-员工添加-来到添加页面

--- spring-boot-04-web-restfulcrud/src/main/java/com/dsxdmfz/controller/EmployeeController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/dsxdmfz/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.dsxdmfz.controller;
 
+import com.dsxdmfz.dao.DepartmentDao;
 import com.dsxdmfz.dao.EmployeeDao;
+import com.dsxdmfz.entities.Department;
 import com.dsxdmfz.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,9 @@ public class EmployeeController {
     @Autowired
     private EmployeeDao employeeDao;
 
+    @Autowired
+    private DepartmentDao departmentDao;
+
     //查询所有员工返回列表页面
     @GetMapping("/emps")
     public String list(Model model){
@@ -27,4 +32,14 @@ public class EmployeeController {
         //classpath:/temlates/xxx.html
         return "emp/list";
     }
+
+    //来到员工添加页面
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+        //来到添加页面，查出所有的部门，在页面显示
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("dpts",departments);
+        return "emp/add";
+    }
+
 }
