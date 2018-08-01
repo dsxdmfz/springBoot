4591271fea2532439115adb8885be6b22e3f77f4
lez <534277194@qq.com>
2018-08-01 11:19:33 +0800

    【实验】-员工添加-添加完成

--- spring-boot-04-web-restfulcrud/src/main/java/com/dsxdmfz/controller/EmployeeController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/dsxdmfz/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -40,6 +41,19 @@ public class EmployeeController {
         Collection<Department> departments = departmentDao.getDepartments();
         model.addAttribute("dpts",departments);
         return "emp/add";
+    }
+
+    //员工添加
+    //springMvc自动请求参数和入参对象的属性进行--绑定；要求请求的参数名字和JavaBean入参对象里面的属性一样
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        System.out.println("保存成功!"+employee);
+        //保存员工
+        employeeDao.save(employee);
+        //来到员工列表页面
+        //redirect：表示重定向到一个地址  /代表当前项目路径
+        //forward:表示转发到一个地址
+        return "redirect:/emps";
     }
 
 }
