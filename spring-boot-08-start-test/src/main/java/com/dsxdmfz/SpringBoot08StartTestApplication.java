ad90a1964b07dee72bf8a221f0cd2c68340994dc
lez <534277194@qq.com>
2018-09-10 10:58:29 +0800

    自定义starter

---
+++ spring-boot-08-start-test/src/main/java/com/dsxdmfz/SpringBoot08StartTestApplication.java
@@ -0,0 +1,12 @@
+package com.dsxdmfz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot08StartTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot08StartTestApplication.class, args);
+	}
+}
---
+++ spring-boot-08-start-test/src/main/java/com/dsxdmfz/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.dsxdmfz.controller;
+
+import com.dsxdmfz.start.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping("/hello")
+    public String hello(){
+        return helloService.sayHello("haha");
+    }
+}
