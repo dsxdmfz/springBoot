f2d2cc1e1ce802571c008819cbdfeca7a848ebb0
dsxdmfz <zhangle.lang@qq.com>
2019-03-20 00:49:15 +0800

    缓存-@Cacheable初体验

--- springboot-01-cache/src/main/java/com/dsxdmfz/Springboot01CacheApplication.java
+++ springboot-01-cache/src/main/java/com/dsxdmfz/Springboot01CacheApplication.java
@@ -3,6 +3,7 @@ package com.dsxdmfz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * 一、搭建基本环境
@@ -12,10 +13,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *  	1、配置数据源信息
  *  	2、使用注解版的Mybatis;
  *  		1)、@MapperScan指定需要扫描的mapper接口所在的包
+ *  二、快速体验缓存
+ *      步骤：
+ *          1、开启基于注解的缓存 @EnableCaching
+ *          2、标注缓存注解即可
+ *
  */
 
 @MapperScan("com.dsxdmfz.mapper")
 @SpringBootApplication
+@EnableCaching
 public class Springboot01CacheApplication {
 
 	public static void main(String[] args) {
