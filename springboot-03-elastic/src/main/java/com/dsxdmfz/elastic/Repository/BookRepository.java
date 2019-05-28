96a687af42aaab959f42286608e13a52b9f2991b
lez <534277194@qq.com>
2019-05-28 17:14:31 +0800

    检索-整合SpringDataElasticsearch

--- springboot-03-elastic/src/main/java/com/dsxdmfz/elastic/Repository/BookRepository.java
+++ springboot-03-elastic/src/main/java/com/dsxdmfz/elastic/Repository/BookRepository.java
@@ -11,6 +11,9 @@ import java.util.List;
  */
 public interface BookRepository extends ElasticsearchRepository<Book,Integer> {
 
+    //自定义方法
+    //参照：https://docs.spring.io/spring-data/elasticsearch/docs/3.1.8.RELEASE/reference/html/
+    //https://docs.spring.io/spring-data/elasticsearch/docs/3.1.8.RELEASE/reference/html/#elasticsearch.query-methods
     List<Book> findByBookNameLike(String bookName);
 
 }
